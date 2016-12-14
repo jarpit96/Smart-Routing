b91ff1a95959161f54ce0a0a2219ee0b9c64404e
Anirudh <anirudhaggarwal2@gmail.com>
2016-12-15 02:51:50 +0530

    error while creating objects,need a fix

--- RouteRecommenderSystem/preprocessing/readFromexcel.py
+++ RouteRecommenderSystem/preprocessing/readFromexcel.py
@@ -1,4 +1,5 @@
 import openpyxl
+from routing.models import State, District, Locality
 
 # Read and store the Excel sheet in sheet object
 wb = openpyxl.load_workbook('delhi_district_wise_localities.xlsx')
@@ -8,10 +9,15 @@ state_column_no = 1
 district_column_no = 2
 locality_column_no = 4
 state_object = {}
+db_state_obj = State.objects.create(name='Delhi')
 
 for i in range(2, sheet.max_row):
     district_name = sheet.cell(row=i, column=district_column_no).value
+    db_district_obj = District.objects.create(name=district_name, state=db_state_obj)
     localities_in_district = str(sheet.cell(row=i, column=locality_column_no).value).split(',')
+    for locality_name in localities_in_district:
+        Locality.objects.create(name=locality_name, district=db_district_obj, crime_wt=0, poi_wt=0,
+                                traffic_wt=0, lat=0, lng=0)
     state_object[district_name] = localities_in_district
 
 print state_object
--- RouteRecommenderSystem/routing/models.py
+++ RouteRecommenderSystem/routing/models.py
@@ -2,19 +2,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 
-class State (models.Model):
-    id = models.AutoField(primary_key = True)
+class State(models.Model):
     name = models.CharField(max_length=200)
 
+
 class District(models.Model):
-    id = models.AutoField(primary_key = True)
     name = models.CharField(max_length=200)
     state = models.ForeignKey('State', on_delete=models.CASCADE)
 
+
 class Locality(models.Model):
-    id = models.AutoField(primary_key = True)
     name = models.CharField(max_length=400)
     district = models.ForeignKey('District', on_delete=models.CASCADE)
     crime_wt = models.FloatField()
