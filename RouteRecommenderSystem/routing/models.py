a55bd53a8bf316c62a71e3fe16eb411ff5db8959
Anirudh <anirudhaggarwal2@gmail.com>
2016-12-15 17:23:29 +0530

    Added geocordinates

--- RouteRecommenderSystem/routing/models.py
+++ RouteRecommenderSystem/routing/models.py
@@ -6,19 +6,28 @@ from django.db import models
 # Create your models here.
 
 class State(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
 
 
 class District(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True)
     state = models.ForeignKey('State', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Locality(models.Model):
-    name = models.CharField(max_length=400)
+    name = models.CharField(max_length=400, null=True)
     district = models.ForeignKey('District', on_delete=models.CASCADE)
-    crime_wt = models.FloatField()
-    poi_wt = models.FloatField()
-    traffic_wt = models.FloatField()
-    lat = models.FloatField()
-    lng = models.FloatField()
+    crime_wt = models.FloatField(default=0.0)
+    poi_wt = models.FloatField(default=0.0)
+    traffic_wt = models.FloatField(default=0.0)
+    lat = models.FloatField(default=0.0)
+    lng = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.name
